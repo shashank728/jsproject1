b4325d0e9f836b25bc605f9ca75f0275b35fb6cd
shashank728 <upadhyayshashank181@gmail.com>
2024-09-10 12:20:53 +0530

    jsproject1

---
+++ script.js
@@ -0,0 +1,26 @@
+var btn = document.querySelector("#add");
+var isstatus = document.querySelector("h5")
+
+var state = 0;
+btn.addEventListener("click",function()
+{
+    if(state ==0)
+    {
+        isstatus.innerHTML = "Friends"
+        isstatus.style.color = "green"
+        btn.innerHTML = "Remove"
+        btn.style.backgroudColor = "grey"
+        state = 1
+
+    }
+    else
+    {
+        isstatus.innerHTML = "strager"
+        isstatus.style.color = "red"
+        state =0
+        
+    }
+
+    
+})
+
